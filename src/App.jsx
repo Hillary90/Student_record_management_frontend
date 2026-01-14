77b25c021c4eed3837199f6cf72fbb9450fa6061
Hillary Tanui <hillarytanui80@gmail.com>
2026-01-14 22:08:04 +0300

    added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import React from 'react'
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-
-  return (
-    <div>
-      <h1>Hello, World!</h1> 
-    </div>
-  )
+  return <AppRoutes />;
 }
 
 export default App
