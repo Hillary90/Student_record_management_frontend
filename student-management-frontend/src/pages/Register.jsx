43253a1d7b0e08a83e2ed2fe99325e7c2c59410d
Hillary Tanui <hillarytanui80@gmail.com>
2026-01-17 19:40:26 +0300

    corrected path

--- student-management-frontend/src/pages/Register.jsx
+++ student-management-frontend/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { useAuth } from '../contexts/ AuthContext'
+import { useAuth } from '../context/AuthContext'
 import Input from '../components/common/Input'
 import Select from '../components/common/Select'
 import Button from '../components/common/Button'
