aedf5e11da96a7ea4c3c6e64ef06871ad7e6fb21
Hillary Tanui <hillarytanui80@gmail.com>
2026-01-17 19:21:34 +0300

    added student services

---
+++ student-management-frontend/src/services/studentService.js
@@ -0,0 +1,28 @@
+import axios from '../api/axios'
+
+export const getAllStudents = async (params = {}) => {
+  const response = await axios.get('/students', { params })
+  return response.data
+}
+
+export const getStudent = async (id, includeGrades = false) => {
+  const response = await axios. get(`/students/${id}`, {
+    params: { include_grades: includeGrades }
+  })
+  return response.data
+}
+
+export const createStudent = async (studentData) => {
+  const response = await axios.post('/students', studentData)
+  return response.data
+}
+
+export const updateStudent = async (id, studentData) => {
+  const response = await axios.put(`/students/${id}`, studentData)
+  return response.data
+}
+
+export const deleteStudent = async (id) => {
+  const response = await axios.delete(`/students/${id}`)
+  return response.data
+}
