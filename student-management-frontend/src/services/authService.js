203ed129bf95c6cf4bed1ba9bb1a740d05d117b3
Hillary Tanui <hillarytanui80@gmail.com>
2026-01-17 19:16:42 +0300

    added auth service

---
+++ student-management-frontend/src/services/authService.js
@@ -0,0 +1,16 @@
+import axios from '../api/axios'
+
+export const login = async (credentials) => {
+  const response = await axios.post('/auth/login', credentials)
+  return response.data
+}
+
+export const register = async (userData) => {
+  const response = await axios. post('/auth/register', userData)
+  return response.data
+}
+
+export const getCurrentUser = async () => {
+  const response = await axios.get('/auth/me')
+  return response.data
+}
