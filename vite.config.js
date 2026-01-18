7284b44926d2da1e5f364a931028cdc4bcfb16b0
Hillary Tanui <hillarytanui80@gmail.com>
2026-01-18 18:16:08 +0300

    fixed deployment

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [
-    react(),
-  ],
-   base: '/Student_record_management_frontend/',
-   server: {
-    port:  5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
+export default defineConfig(({ command, mode }) => {
+  
+  const base = mode === 'production' 
+    ?  '/Student_record_management_frontend/' 
+    : '/'
+
+  return {
+    plugins: [react()],
+    base: base,
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5000',
+          changeOrigin: true,
+        },
       },
     },
-  },
+  }
 })
